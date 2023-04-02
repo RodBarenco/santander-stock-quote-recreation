5b2f073169e283698c278c2c975b75e78cd014d3
RodBarenco <101021878+RodBarenco@users.noreply.github.com>
2023-04-02 14:02:30 -0300

    Update StockController.java

--- src/main/java/com/project/besantanderstockquoterecreation/controller/StockController.java
+++ src/main/java/com/project/besantanderstockquoterecreation/controller/StockController.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://rodbarenco.github.io")
 @RestController
 @RequestMapping(value = "/stock")
 public class StockController {
@@ -23,38 +23,31 @@ public class StockController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto, HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.save(dto));
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.save(dto));
     }
 
     @PutMapping(consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto, HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.update(dto));
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.update(dto));
     }
 
     @GetMapping(produces =  MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StockDTO>> findAll(HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.findAll());
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.findAll());
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> findById(@PathVariable Long id, HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.findById(id));
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.findById(id));
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> delete(@PathVariable Long id, HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.delete(id));
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.delete(id));
     }
 
     @GetMapping(value = "/today", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StockDTO>> findByToday(HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "https://rodbarenco.github.io");
-        return ResponseEntity.ok(service.findByToday());
-
+        return ResponseEntity.ok().header("Access-Control-Allow-Origin", "*").body(service.findByToday());
     }
 }
